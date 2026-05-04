e984c91a7d2f52ced75d6994ecfc50e11191bb1b
zonatedbootsrealm <bilal_agboatwala@es-impact.org>
2026-05-04 13:44:55 -0700

    Update hub.js

--- hub.js
+++ hub.js
@@ -1,13 +1,6 @@
 function travelDimension(place) {
   console.log("function works");
-  const portal = document.getElementById("portal")
-  document.body.classList.add("fade-out");
-  document.getElementById("overlay").style.opacity = "1";
-  portal.play()
-
-  setTimeout(() => {
-    window.location.href = place;
-  }, 601);
+  
 }
 
 document.addEventListener("keydown", function(event) {
