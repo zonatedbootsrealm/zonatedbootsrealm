7a78bfbcabbb487e0b2900b1432782dd3dd22a0f
zonatedbootsrealm <bilal_agboatwala@es-impact.org>
2026-05-04 13:32:01 -0700

    Update hub.js

--- hub.js
+++ hub.js
@@ -1,1 +1,10 @@
+function travelDimension() {
+  const portal = document.getElementById("portal")
+  document.body.classList.add("fade-out");
+  document.getElementById("overlay").style.opacity = "1";
+  portal.play()
 
+  setTimeout(() => {
+    window.location.href = "games/platformer/game.html";
+  }, 601);
+}
