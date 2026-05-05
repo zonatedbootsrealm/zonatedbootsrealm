a8533eb093030dec727b1df6c254c3630847d040
zonatedbootsrealm <bilal_agboatwala@es-impact.org>
2026-05-04 20:33:33 -0700

    Update platformerhub.js

--- games/platformer/platformerhub.js
+++ games/platformer/platformerhub.js
@@ -9,9 +9,17 @@ function travelDimension(place) {
   }, 5);
 }
 
+function SaveStuffPlatformer() {
+  localStorage.setItem("playerX", player.x)
+  localStorage.setItem("playerY", player.y)
+  localStorage.setItem("cameraX", camera.x)
+  localStorage.setItem("cameraY", camera.y)
+}
+
 document.addEventListener("keydown", function(event) {
 
   if (event.key === "Escape") {
+    SaveStuffPlatformer()
     travelDimension("/zonatedbootsrealm/index.html")
   }
 
